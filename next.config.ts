28b49bedb31a9da06c0e7e9fa620a87029b0b903
Chimaera <banuchi87@gmail.com>
2025-04-17 14:42:49 -0400

    2025-04-17: Deploy Attempts - update build config, move to sub root url path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  
 };
 
 export default nextConfig;
