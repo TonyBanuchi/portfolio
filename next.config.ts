436bc03c53e285d7c41265ee301268a7fe7bd26a
Chimaera <banuchi87@gmail.com>
2025-04-28 10:02:10 -0400

    2025-04-28: Update Next.js config with note annotations.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  reactStrictMode: true,
+  output: "export", // Configure for static content delivery, build goes to "out" folder.
+  reactStrictMode: true, // default when using app Router
   images: {
-    unoptimized: true,
+    unoptimized: true, // Static Content conflicts with optimization, so no need to waste resources doing it.
   },
   
 };
